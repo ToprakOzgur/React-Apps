724eff73a39a8346d77cf164658c5fcc5672cb41
ToprakOzgur <zgrtprk@gmail.com>
2020-08-04 13:38:31 +0300

    todo-list app finished

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -45,7 +45,15 @@ class App extends Component {
   };
 
   handleEdit = (id) => {
-    console.log(`handle edit ${id}`);
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    const selectedItem = this.state.items.find((item) => item.id === id);
+
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true,
+    });
   };
 
   render() {
@@ -58,7 +66,7 @@ class App extends Component {
               item={this.state.item}
               handleChange={this.handleChange}
               handleSubmit={this.handleSubmit}
-              editItem={this.editItem}
+              editItem={this.state.editItem}
             />
             <TodoList
               items={this.state.items}
