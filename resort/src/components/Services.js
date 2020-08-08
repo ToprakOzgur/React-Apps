05c0a9f9f6259ff91b0f020ce95396b5b7fb4d1e
ToprakOzgur <zgrtprk@gmail.com>
2020-08-08 14:49:08 +0300

    resort app services comp

---
+++ resort/src/components/Services.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Title from "./Title";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "Free Cocktails",
+        info:
+          "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, cupiditate.",
+      },
+      {
+        icon: <FaHiking />,
+        title: "Endless Hiking",
+        info:
+          "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, cupiditate.",
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "free shuttle",
+        info:
+          "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, cupiditate.",
+      },
+      {
+        icon: <FaBeer />,
+        title: "Strongest Beer",
+        info:
+          "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, cupiditate.",
+      },
+    ],
+  };
+  render() {
+    return (
+      <section className="services">
+        <Title title="services"></Title>
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+import Services from "../components/Services";
+
 import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <Hero>
-      <Banner title="luxurious rooms" subTitle="deluxe rooms starting ar $299">
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subTitle="deluxe rooms starting ar $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services></Services>
+    </>
   );
 }
