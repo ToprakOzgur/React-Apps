2c19f74487c0e7d1bb25f41fad9c316ce9062da2
ToprakOzgur <zgrtprk@gmail.com>
2020-08-08 12:48:09 +0300

    resort app navbar

--- resort/src/App.js
+++ resort/src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
 
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <>hello from App</>;
+  return (
+    <>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
