a449d56f4bf56e49b2b506a6e519622b3d517599
ToprakOzgur <zgrtprk@gmail.com>
2020-08-21 08:55:38 +0300

    budget app expanseitem comp

--- budget-calculator/src/components/ExpenseItem.js
+++ budget-calculator/src/components/ExpenseItem.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { MdEdit, MdDelete } from "react-icons/md";
+const ExpenseItem = ({ expense }) => {
+  const { id, charge, amount } = expense;
 
-const ExpenseItem = () => {
-  return <div>Hello from expenseItem</div>;
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expanse">{charge}</span>
+        <span className="amount">{amount}</span>
+      </div>
+      <button className="edit-btn" aria-label="edit button">
+        <MdEdit />
+      </button>
+      <button className="clear-btn" aria-label="clear button">
+        <MdDelete />
+      </button>
+    </li>
+  );
 };
 export default ExpenseItem;
