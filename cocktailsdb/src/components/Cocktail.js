eb44e07a4cf68df2a6b3d3e154929857cc598eba
ToprakOzgur <zgrtprk@gmail.com>
2020-08-22 22:16:22 +0300

    cocktail app completed

--- cocktailsdb/src/components/Cocktail.js
+++ cocktailsdb/src/components/Cocktail.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Cocktail() {
-  return <h1>cocktail component</h1>;
+export default function Cocktail({ info, name, id, image, glass }) {
+  return (
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="container-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={`/cocktails/${id}`} className="btn-primary btn btn-details">
+          details
+        </Link>
+      </div>
+    </article>
+  );
 }
